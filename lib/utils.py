7a4adf9a18065551b8c4e6789a3c402ca48515b6
Brunna Arruda <brunnaarrudab@gmail.com>
2020-04-21 15:21:39 -0300

    utils
    utils commit

---
+++ lib/utils.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+
+class BrowserUtils:
+    def __init__(self):
+        self.browser = None
+
+    def open_page(self, browser='chrome', link=None):
+        # open page
+        if browser == 'firefox':
+            self.browser= webdriver.Firefox()
+        elif browser == 'chrome':
+            self.browser = webdriver.Chrome()
+        self.browser.get(link)
+        return self.browser
+
+    def close_page(self, browser):
+        # close one tab
+        browser.close()
+
+    def quit_page(self, browser):
+        # close all tabs
+        browser.quit()
